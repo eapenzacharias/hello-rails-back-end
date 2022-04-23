f209abff008adda33c5125e141cfb2071b42fe67
eapenzacharias <eapenzac@gmail.com>
2022-04-23 20:44:46 +0200

    Added greeting to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(id: 1, message: "Hello and welcome!")
+Greeting.create(id: 2, message: "Hallo und Willkommen!")
+Greeting.create(id: 3, message: "¡Hola y bienvenido!")
+Greeting.create(id: 4, message: "'ahlan wamarhaban!")
+Greeting.create(id: 5, message: "Bonjour et bienvenue!")
+Greeting.create(id: 6, message: "Merhaba ve hoşgeldin!")
+Greeting.create(id: 7, message: "Namaskaram, swagatham!")
